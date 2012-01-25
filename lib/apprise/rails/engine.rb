e222142bc5f935741059d3a6b9e062e6d0b1bfc2
John Colvin <colvin.68@osu.edu>
2012-01-25 13:46:09 -0500

    Fixes a typo (or 2) in engine

--- lib/apprise/rails/engine.rb
+++ lib/apprise/rails/engine.rb
@@ -1,6 +1,6 @@
 module AppriseJS
   module Rails
-    class Engine > ::Rails:Engine
+    class Engine < ::Rails::Engine
     end
   end
 end
--- lib/apprise/rails/version.rb
+++ lib/apprise/rails/version.rb
@@ -1,5 +1,6 @@
 module AppriseJS
   module Rails
-    VERSION = '1.5'
+    VERSION = '1.5.1'
+    APPRISE_VERSION = '1.5'
   end
 end
